9a117212047632d663eca8958f2653d05f912ee6
Pramod Teewaree <prteewaree@gmail.com>
2018-09-06 21:31:53 +0400

    New version Android Hello World Java from PrMod

---
+++ app/src/main/java/com/pramoduniverse/helloworldjava/MainActivity.java
@@ -0,0 +1,38 @@
+package com.pramoduniverse.helloworldjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonGreet = (Button) findViewById(R.id.buttonGreet);
+        buttonGreet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editFullName = (EditText) findViewById(R.id.editFullName);
+                TextView textOutput = (TextView) findViewById(R.id.textOutput);
+
+                String strFullName = editFullName.getText().toString();
+                String strOutput;
+                if(!strFullName.isEmpty()) {
+                    strOutput = getResources().getString(R.string.hello_text) + " " +
+                            strFullName.trim() + getResources().getString(R.string.welcome_text);
+                } else {
+                    strOutput = getResources().getString(R.string.hello_init);
+                }
+
+                textOutput.setText(strOutput);
+            }
+        });
+
+    }
+}
